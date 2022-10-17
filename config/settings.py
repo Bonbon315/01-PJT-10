d2302a0e0d521d57f7624b4dbb2b0348d1d40cca
Bonbon315 <n202u123@gmail.com>
2022-10-17 16:06:23 +0900

    image upload

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+# Media files
+MEDIA_ROOT = BASE_DIR / "images"
+MEDIA_URL = "/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
